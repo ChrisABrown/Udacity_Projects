82a64a4269cef3508c3ddbf17092a62bd0d1d6a4
Chris Brown <102696425+ChrisABrown@users.noreply.github.com>
2023-08-18 20:37:50 -0500

    built function to get specific product from products array, refactoring following functions to deal with adjustments

--- Shopping_Cart/starterCode/starter/src/assets/script.js
+++ Shopping_Cart/starterCode/starter/src/assets/script.js
@@ -12,26 +12,41 @@ const products = [
     name: 'Oranges',
     price: 5,
     quantity: 0,
-    productId: productId,
+    productId: productId + 1,
     image: '../images/orange.jpg',
   },
   {
     name: 'Strawberries',
     price: 5,
     quantity: 0,
-    productId: productId,
+    productId: productId + 2,
     image: '../images/strawberry.jpg',
   },
 ]
 
 const cart = []
+const getProduct = (productId) => {
+  for (let i = 0; i < products.length; i++) {
+    let item = products[i]
+    if (item.productId === productId) return item
+  }
+}
+
 const addProductToCart = (productId) => {
-  let item = products[productId]
-  if (item in cart) {
-    products[productId].quantity += 1
-  } else {
-    products[productId].quantity += 1
+  let item = getProduct(productId)
+  if (item.productId === productId && !(item in cart)) {
+    item.quantity += 1
     cart.push(item)
+  } else {
+    item.quantity += 1
+  }
+}
+
+const removeProductFromCart = (productId) => {
+  let item = getProduct(productId)
+  if (item in cart) {
+    item.quantity = 0
+    cart.splice(products.indexOf(item), 1)
   }
 }
 
@@ -45,39 +60,41 @@ const decreaseQuantity = (productId) => {
   let index = item.findIndex()
   if (item.quantity === 0 && item in cart) {
     cart.splice(index, 1, item)
-  } else if (item.quantity > 0) {
+  } else if (item.quantity > 0 && item in cart) {
     item.quantity -= 1
   }
 }
-
-/* Create a function named increaseQuantity that takes in the productId as an argument
-  - increaseQuantity should get the correct product based on the productId
-  - increaseQuantity should then increase the product's quantity
+/* Create a function named pay that takes in an amount as an argument
+  - pay will return a negative number if there is a remaining balance
+  - pay will return a positive number if money should be returned to customer
 */
-
-/* Create a function named decreaseQuantity that takes in the productId as an argument
-  - decreaseQuantity should get the correct product based on the productId
-  - decreaseQuantity should decrease the quantity of the product
-  - if the function decreases the quantity to 0, the product is removed from the cart
-*/
-
-/* Create a function named removeProductFromCart that takes in the productId as an argument
-  - removeProductFromCart should get the correct product based on the productId
-  - removeProductFromCart should update the product quantity to 0
-  - removeProductFromCart should remove the product from the cart
-*/
+const pay = (amount) => {
+  let total = cartTotal()
+  if (amount < total) {
+    return amount - total
+  } else if (amount === total) {
+    return 0
+  } else {
+    return amount - total
+  }
+}
 
 /* Create a function named cartTotal that has no parameters
   - cartTotal should iterate through the cart to get the total of all products
   - cartTotal should return the sum of the products in the cart
 */
+const cartTotal = () => {
+  let total
+  cart.forEach((el) => {
+    total += el['price']
+  })
+  return total
+}
 
 /* Create a function called emptyCart that empties the products from the cart */
-
-/* Create a function named pay that takes in an amount as an argument
-  - pay will return a negative number if there is a remaining balance
-  - pay will return a positive number if money should be returned to customer
-*/
+const emptyCart = () => {
+  cart.splice(0, cart.length)
+}
 
 /* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
